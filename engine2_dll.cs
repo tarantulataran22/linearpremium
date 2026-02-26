5e62dcbbe2fec463893705545b0fd00f4b225a56
!`Script <76524734+tarantulataran22@users.noreply.github.com>
2026-02-26 03:16:18 +0300

    Add files via upload

--- engine2_dll.cs
+++ engine2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-24 00:17:51.026197700 UTC
+// 2026-02-26 00:14:46.084740900 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: engine2.dll
--- rendersystemdx11_dll.rs
+++ rendersystemdx11_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-24 00:17:51.026197700 UTC
+// 2026-02-26 00:14:46.084740900 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
