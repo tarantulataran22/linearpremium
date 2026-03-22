a848ff44f3c745fbc604ecd2008ce0c6326ebdf1
!`Script <76524734+tarantulataran22@users.noreply.github.com>
2026-03-23 02:38:43 +0300

    Add files via upload

--- server_dll.cs
+++ server_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-19 10:05:24.652271200 UTC
+// 2026-03-22 23:37:08.794260 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: server.dll
