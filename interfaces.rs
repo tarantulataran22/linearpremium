57a23896920a2677634dad23c2d3730078162271
!`Script <76524734+tarantulataran22@users.noreply.github.com>
2026-04-03 15:09:07 +0300

    Add files via upload

--- interfaces.rs
+++ interfaces.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-31 18:58:54.400879900 UTC
+// 2026-04-03 12:05:58.221067300 UTC
 
 #![allow(non_upper_case_globals, unused)]
 
@@ -12,14 +12,14 @@ pub mod cs2_dumper {
         }
         // Module: client.dll
         pub mod client_dll {
-            pub const ClientToolsInfo_001: usize = 0x2062F90;
-            pub const EmptyWorldService001_Client: usize = 0x201CE60;
-            pub const GameClientExports001: usize = 0x205FC70;
-            pub const LegacyGameUI001: usize = 0x207D4D0;
-            pub const Source2Client002: usize = 0x2309DD0;
-            pub const Source2ClientConfig001: usize = 0x228DF00;
-            pub const Source2ClientPrediction001: usize = 0x206A8F0;
-            pub const Source2ClientUI001: usize = 0x207BD60;
+            pub const ClientToolsInfo_001: usize = 0x2065F90;
+            pub const EmptyWorldService001_Client: usize = 0x201FE50;
+            pub const GameClientExports001: usize = 0x2062C70;
+            pub const LegacyGameUI001: usize = 0x20804D0;
+            pub const Source2Client002: usize = 0x230CDD0;
+            pub const Source2ClientConfig001: usize = 0x2290F00;
+            pub const Source2ClientPrediction001: usize = 0x206D8F0;
+            pub const Source2ClientUI001: usize = 0x207ED60;
         }
         // Module: engine2.dll
         pub mod engine2_dll {
